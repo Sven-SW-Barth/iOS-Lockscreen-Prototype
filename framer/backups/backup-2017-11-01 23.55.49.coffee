370c8f3adf30788756a2c81b8626ea722711b848
lucrka <luca.stetter@t-online.de>
2017-11-02 22:45:59 +0100

    summary
    summary description

---
+++ framer/backups/backup-2017-11-01 23.55.49.coffee
@@ -0,0 +1,272 @@
+# Import file "ios lockscreen prototype"
+sketch = Framer.Importer.load("imported/ios%20lockscreen%20prototype@3x", scale: 1)
+
+codeR = "1234"
+code = ""
+
+codePoints = [sketch.code1, sketch.code2, sketch.code3, sketch.code4]
+
+for i in codePoints
+	i.visible = true
+	i.opacity = 0
+
+darker.opacity = 0
+
+sketch.background.visible = false;
+sketch.home.visible = false;
+sketch.background.visible = false
+sketch.unlocked.opacity = 0;
+
+sketch.unlock.backgroundColor = "rgba(0, 0, 0, 0)"
+sketch.unlocked.backgroundColor = "rgba(0, 0, 0, 0)"
+
+
+	
+sketch.Group.opacity = 0.25
+sketch.Group_2.opacity = 0.25
+sketch.Group_3.opacity = 0.25
+sketch.Group_4.opacity = 0.25
+sketch.Group_5.opacity = 0.25
+sketch.Group_6.opacity = 0.25
+sketch.Group_7.opacity = 0.25
+sketch.Group_8.opacity = 0.25
+sketch.Group_9.opacity = 0.25
+sketch.Group_10.opacity = 0.25
+
+pager = new PageComponent
+	size: Screen.size
+	scrollVertical: false
+
+homeC = new Layer
+	width: Screen.width
+	height: Screen.height
+	image: "images/home.png"
+	parent: pager.content
+	
+
+statusbar = new Layer
+	width: Screen.width
+	height: Screen.height
+	image: "images/statusbar.png"
+
+
+pager.addPage(sketch.unlock)
+
+check = ->
+	print code.length
+	
+	drawDots()
+				
+	if code.length > 3
+		if code == codeR
+			print "right"
+			unlock()
+		else
+			
+# 			wrongCode()
+# 			for i in codePoints
+# 				i.visible = false
+			drawDots()
+			deleteDots()
+			code = ""
+			
+	
+
+drawDots = ->
+	if code.length > 0
+		for i in [0..code.length-1]
+			codePoints[i].opacity = 1
+	else if code.length < 4
+		for i in [code.length..3]
+			codePoints[i].opacity = 0
+			
+deleteDots = ->
+	for i in [0..3]
+			codePoints[i].animate
+				opacity: 0
+	
+
+sketch.$1.onTapStart ->
+	code += 1
+	sketch.Group.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$1.onTapEnd ->
+	sketch.Group.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+	
+sketch.$2.onTapStart ->
+	code += 2
+	sketch.Group_2.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$2.onTapEnd ->
+	sketch.Group_2.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.$3.onTapStart ->
+	code += 3
+	sketch.Group_3.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$3.onTapEnd ->
+	sketch.Group_3.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+	
+sketch.$4.onTapStart ->
+	code += 4
+	sketch.Group_4.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$4.onTapEnd ->
+	sketch.Group_4.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+	
+sketch.$5.onTapStart ->
+	code += 5
+	sketch.Group_5.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$5.onTapEnd ->
+	sketch.Group_5.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.$6.onTapStart ->
+	code += 6
+	sketch.Group_6.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$6.onTapEnd ->
+	sketch.Group_6.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.$7.onTapStart ->
+	code += 7
+	sketch.Group_7.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$7.onTapEnd ->
+	sketch.Group_7.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.$8.onTapStart ->
+	code += 8
+	sketch.Group_8.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$8.onTapEnd ->
+	sketch.Group_8.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+	
+sketch.$9.onTapStart ->
+	code += 9
+	sketch.Group_9.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$9.onTapEnd ->
+	sketch.Group_9.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.$0.onTapStart ->
+	code += 1
+	sketch.Group_10.animate
+		opacity: 1
+		options:
+			time: 0.1
+			curve: Bezier.ease
+
+sketch.$0.onTapEnd ->
+	sketch.Group_10.animate
+		opacity: 0.25
+		options:
+			time: 0.4
+			curve: Bezier.ease
+	check()
+
+sketch.cancel.onTap ->
+	pager.snapToPage(homeC)
+	code = ""
+	drawDots()
+	
+	
+pager.content.on "change:x", ->
+	hintergrund.blur = Utils.modulate this.x, [0, -375], [0, 5]
+	hintergrund.scale = Utils.modulate this.x, [0, -375], [1, 1.1]
+	darker.opacity = Utils.modulate this.x, [0, -375], [0, 0.3]
+
+
+
+unlock = ->
+	sketch.unlock.animate
+		maxY: 0
+	hintergrund.animate
+		blur: 0
+		scale: 1
+	darker.animate
+		opacity: 0
+		options:
+			time: 0.4
+	
---
+++ imported/ios lockscreen prototype@3x/layers.json.js
@@ -0,0 +1,932 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["ios lockscreen prototype@3x/layers.json.js"] = [
+	{
+		"objectId": "01C345B4-56FF-476C-AD5B-AE3840839A31",
+		"kind": "artboard",
+		"name": "home",
+		"originalName": "home",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": -287,
+			"y": -22,
+			"width": 375,
+			"height": 667
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"image": {
+			"path": "images/Layer-home-mdfdmzq1.png",
+			"frame": {
+				"x": -287,
+				"y": -22,
+				"width": 375,
+				"height": 667
+			}
+		},
+		"children": [
+			{
+				"objectId": "C20FE007-1342-4F7B-8E92-8BF1372E2637",
+				"kind": "group",
+				"name": "Slider_Dots",
+				"originalName": "Slider Dots",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 166,
+					"y": 651,
+					"width": 46,
+					"height": 10
+				},
+				"visible": false,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Slider_Dots-qziwrkuw.png",
+					"frame": {
+						"x": 166,
+						"y": 651,
+						"width": 46,
+						"height": 10
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "5F59FA6C-FBA4-42E6-8885-74628E49A173",
+				"kind": "group",
+				"name": "Date",
+				"originalName": "Date",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 110,
+					"y": 71,
+					"width": 154,
+					"height": 194
+				},
+				"visible": false,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Date-nuy1ouzb.png",
+					"frame": {
+						"x": 110,
+						"y": 71,
+						"width": 154,
+						"height": 194
+					}
+				},
+				"children": []
+			}
+		]
+	},
+	{
+		"objectId": "642BF35B-A249-42F4-88EB-B63226A42F91",
+		"kind": "artboard",
+		"name": "unlock",
+		"originalName": "unlock",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": -287,
+			"y": 1017,
+			"width": 375,
+			"height": 667
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"image": {
+			"path": "images/Layer-unlock-njqyqkyz.png",
+			"frame": {
+				"x": -287,
+				"y": 1017,
+				"width": 375,
+				"height": 667
+			}
+		},
+		"children": [
+			{
+				"objectId": "B87176EF-253C-4053-9688-008B10934F44",
+				"kind": "group",
+				"name": "Dots",
+				"originalName": "Dots",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 122,
+					"y": 139,
+					"width": 131,
+					"height": 15
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Dots-qjg3mtc2.png",
+					"frame": {
+						"x": 122,
+						"y": 139,
+						"width": 131,
+						"height": 15
+					}
+				},
+				"children": [
+					{
+						"objectId": "8F062BB1-F5B5-4BFB-8B3B-1C8CA15C2F35",
+						"kind": "group",
+						"name": "code4",
+						"originalName": "code4",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 238,
+							"y": 139,
+							"width": 15,
+							"height": 15
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-code4-oeywnjjc.png",
+							"frame": {
+								"x": 238,
+								"y": 139,
+								"width": 15,
+								"height": 15
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "11FB7555-CD62-428E-8135-8CB69A90888C",
+						"kind": "group",
+						"name": "code2",
+						"originalName": "code2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 161,
+							"y": 140,
+							"width": 13,
+							"height": 13
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-code2-mtfgqjc1.png",
+							"frame": {
+								"x": 161,
+								"y": 140,
+								"width": 13,
+								"height": 13
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "5475F32A-6AA8-4305-8EAD-3508F4DA6B1E",
+						"kind": "group",
+						"name": "code3",
+						"originalName": "code3",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 200,
+							"y": 140,
+							"width": 13,
+							"height": 13
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-code3-ntq3nuyz.png",
+							"frame": {
+								"x": 200,
+								"y": 140,
+								"width": 13,
+								"height": 13
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "37982282-BD99-4F0C-8AD6-E4D8B1EA7B8B",
+						"kind": "group",
+						"name": "code1",
+						"originalName": "code1",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 122,
+							"y": 140,
+							"width": 13,
+							"height": 13
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-code1-mzc5odiy.png",
+							"frame": {
+								"x": 122,
+								"y": 140,
+								"width": 13,
+								"height": 13
+							}
+						},
+						"children": []
+					}
+				]
+			},
+			{
+				"objectId": "0004F3CC-4E0A-4CE9-8371-251F15B44832",
+				"kind": "group",
+				"name": "Keyboard",
+				"originalName": "Keyboard",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 47,
+					"y": 206,
+					"width": 281,
+					"height": 345
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"children": [
+					{
+						"objectId": "14064B45-EA98-409A-9128-914A567BC1F8",
+						"kind": "group",
+						"name": "$1",
+						"originalName": "1",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 47,
+							"y": 206,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$1-mtqwnjrc.png",
+							"frame": {
+								"x": 47,
+								"y": 206,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "A46B054D-A2E9-4355-9CC6-D376E230A318",
+								"kind": "group",
+								"name": "Group",
+								"originalName": "Group",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 47,
+									"y": 206,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group-qtq2qja1.png",
+									"frame": {
+										"x": 47,
+										"y": 206,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "49B4B777-059A-4480-B9FE-FD9DE39C53E6",
+						"kind": "group",
+						"name": "$2",
+						"originalName": "2",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 150,
+							"y": 206,
+							"width": 76,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$2-ndlcnei3.png",
+							"frame": {
+								"x": 150,
+								"y": 206,
+								"width": 76,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "02E053EF-33DE-4DCE-A31C-AF4334EEBA8E",
+								"kind": "group",
+								"name": "Group_2",
+								"originalName": "Group 2",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 150.5,
+									"y": 206,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_2-mdjfmduz.png",
+									"frame": {
+										"x": 150.5,
+										"y": 206,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "64844BEF-23E9-42E6-8B94-5411770A7FA1",
+						"kind": "group",
+						"name": "$3",
+						"originalName": "3",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 253,
+							"y": 206,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$3-njq4ndrc.png",
+							"frame": {
+								"x": 253,
+								"y": 206,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "0989F08B-4915-41FE-868E-BAD49B1F3429",
+								"kind": "group",
+								"name": "Group_3",
+								"originalName": "Group 3",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 253,
+									"y": 206,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_3-mdk4ouyw.png",
+									"frame": {
+										"x": 253,
+										"y": 206,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "B0906C52-0E71-46BD-B150-9733F52F5504",
+						"kind": "group",
+						"name": "$4",
+						"originalName": "4",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 47,
+							"y": 296,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$4-qja5mdzd.png",
+							"frame": {
+								"x": 47,
+								"y": 296,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "04CFD194-AA00-4BFE-B205-5982DD704804",
+								"kind": "group",
+								"name": "Group_4",
+								"originalName": "Group 4",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 47,
+									"y": 296,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_4-mdrdrkqx.png",
+									"frame": {
+										"x": 47,
+										"y": 296,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "7DE6D7F5-2CFC-47D7-ACE4-D380C46DAE6A",
+						"kind": "group",
+						"name": "$5",
+						"originalName": "5",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 150,
+							"y": 296,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$5-n0rfnkq3.png",
+							"frame": {
+								"x": 150,
+								"y": 296,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "6CB70287-01B4-4044-9BAE-6A6C11ADE1C6",
+								"kind": "group",
+								"name": "Group_5",
+								"originalName": "Group 5",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 150,
+									"y": 296,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_5-nkncnzay.png",
+									"frame": {
+										"x": 150,
+										"y": 296,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "7D95369C-71AA-4B63-88A6-99D92A51426C",
+						"kind": "group",
+						"name": "$6",
+						"originalName": "6",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 253,
+							"y": 296,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$6-n0q5ntm2.png",
+							"frame": {
+								"x": 253,
+								"y": 296,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "DCA3A806-826F-4366-913E-42D0A018F352",
+								"kind": "group",
+								"name": "Group_6",
+								"originalName": "Group 6",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 253,
+									"y": 296,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_6-renbm0e4.png",
+									"frame": {
+										"x": 253,
+										"y": 296,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "5A0FE8BF-CC2F-46C3-9237-1F58C50CCBDA",
+						"kind": "group",
+						"name": "$7",
+						"originalName": "7",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 47,
+							"y": 386,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$7-nuewrku4.png",
+							"frame": {
+								"x": 47,
+								"y": 386,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "0B82B503-9BB3-4C17-AD19-A99342D636FD",
+								"kind": "group",
+								"name": "Group_7",
+								"originalName": "Group 7",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 47,
+									"y": 386,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_7-mei4mki1.png",
+									"frame": {
+										"x": 47,
+										"y": 386,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "9AD51ACB-B002-4113-99FD-AB8DF51E1F21",
+						"kind": "group",
+						"name": "$8",
+						"originalName": "8",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 150,
+							"y": 386,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$8-oufentfb.png",
+							"frame": {
+								"x": 150,
+								"y": 386,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "180670D9-24D6-42C1-8391-58AED5C45B9D",
+								"kind": "group",
+								"name": "Group_8",
+								"originalName": "Group 8",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 150,
+									"y": 386,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_8-mtgwnjcw.png",
+									"frame": {
+										"x": 150,
+										"y": 386,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "A82D63B7-A3CB-459E-A137-9D11890387DD",
+						"kind": "group",
+						"name": "$9",
+						"originalName": "9",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 253,
+							"y": 386,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$9-qtgyrdyz.png",
+							"frame": {
+								"x": 253,
+								"y": 386,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "94DB9265-ECFD-4D94-8C9D-062B3CCABE3E",
+								"kind": "group",
+								"name": "Group_9",
+								"originalName": "Group 9",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 253,
+									"y": 386,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_9-otreqjky.png",
+									"frame": {
+										"x": 253,
+										"y": 386,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					},
+					{
+						"objectId": "D48A3514-F880-4888-9FAC-B58E2700DE8B",
+						"kind": "group",
+						"name": "$0",
+						"originalName": "0",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 150,
+							"y": 476,
+							"width": 75,
+							"height": 75
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-$0-rdq4qtm1.png",
+							"frame": {
+								"x": 150,
+								"y": 476,
+								"width": 75,
+								"height": 75
+							}
+						},
+						"children": [
+							{
+								"objectId": "C75483C5-468E-4585-BBAC-1761E2473A00",
+								"kind": "group",
+								"name": "Group_10",
+								"originalName": "Group 10",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 150,
+									"y": 476,
+									"width": 75,
+									"height": 75
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-Group_10-qzc1ndgz.png",
+									"frame": {
+										"x": 150,
+										"y": 476,
+										"width": 75,
+										"height": 75
+									}
+								},
+								"children": []
+							}
+						]
+					}
+				]
+			},
+			{
+				"objectId": "7B783428-3AC1-4CDA-93EE-1F752F9A77D8",
+				"kind": "text",
+				"name": "cancel",
+				"originalName": "cancel",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 267,
+					"y": 616,
+					"width": 48,
+					"height": 14
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1,
+					"string": "Cancel",
+					"css": [
+						"/* Cancel: */",
+						"font-family: SFProText-Regular;",
+						"font-size: 16px;",
+						"color: #FFFFFF;",
+						"letter-spacing: -0.39px;"
+					]
+				},
+				"image": {
+					"path": "images/Layer-cancel-n0i3odm0.png",
+					"frame": {
+						"x": 267,
+						"y": 616,
+						"width": 48,
+						"height": 14
+					}
+				},
+				"children": []
+			}
+		]
+	},
+	{
+		"objectId": "E5B92917-8EF6-4AED-BFB1-4DD8CE576372",
+		"kind": "artboard",
+		"name": "background",
+		"originalName": "background",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 146,
+			"y": -22,
+			"width": 375,
+			"height": 667
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"image": {
+			"path": "images/Layer-background-rtvcoti5.png",
+			"frame": {
+				"x": 146,
+				"y": -22,
+				"width": 375,
+				"height": 667
+			}
+		},
+		"children": [
+			{
+				"objectId": "17A8045F-B191-4B3D-B3DD-52BEAFEAD166",
+				"kind": "group",
+				"name": "Slider_Dots1",
+				"originalName": "Slider Dots",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 166,
+					"y": 652,
+					"width": 46,
+					"height": 9
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Slider_Dots-mtdboda0.png",
+					"frame": {
+						"x": 166,
+						"y": 652,
+						"width": 46,
+						"height": 9
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "8DB08517-0BEC-4D08-92F7-88239A401E6C",
+				"kind": "group",
+				"name": "Date1",
+				"originalName": "Date",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 111,
+					"y": 82,
+					"width": 152,
+					"height": 183
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"image": {
+					"path": "images/Layer-Date-oercmdg1.png",
+					"frame": {
+						"x": 111,
+						"y": 82,
+						"width": 152,
+						"height": 183
+					}
+				},
+				"children": []
+			}
+		]
+	}
+]
